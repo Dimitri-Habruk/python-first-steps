429dc531aa55da92418dd1d114c7fceb4ee3e1da
Dimitri <d.habruk@gmail.com>
2024-11-29 18:01:36 +0100

    input

--- firstfile.py
+++ firstfile.py
@@ -1,4 +1,4 @@
-import math
+# import math
 # print(math.ceil(5.2))
 # print(math.floor(5.2))
 # print(math.sqrt(25))
@@ -6,11 +6,17 @@ import math
 # the another most useful function from math library are the following functions
 # math.pow(x,y) - returns x raised to the power y
 # math.log(x) - returns the natural logarithm of x
+
 # print("first", end="++")
 # print("second")
+
 x = 5.5
 y = round(98.22)
 print("coortinates are: x = ", x , "; y = ", y), 
 print("coortinates are: x = ", x , "; y = ", y, sep=""), 
-#F-строки с пайтона 3.6
+
+#F-from python 3.6
 print(f"corridantes: x= {x}; y = {y}")
+
+name = input()
+print(name, type(name))
