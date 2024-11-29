91a8fd562292c71a78391df21cd8c79eb13cfa41
Dimitri <d.habruk@gmail.com>
2024-11-29 17:54:20 +0100

    first file

---
+++ firstfile.py
@@ -0,0 +1,16 @@
+import math
+# print(math.ceil(5.2))
+# print(math.floor(5.2))
+# print(math.sqrt(25))
+# print(math.factorial(4))
+# the another most useful function from math library are the following functions
+# math.pow(x,y) - returns x raised to the power y
+# math.log(x) - returns the natural logarithm of x
+# print("first", end="++")
+# print("second")
+x = 5.5
+y = round(98.22)
+print("coortinates are: x = ", x , "; y = ", y), 
+print("coortinates are: x = ", x , "; y = ", y, sep=""), 
+#F-строки с пайтона 3.6
+print(f"corridantes: x= {x}; y = {y}")
